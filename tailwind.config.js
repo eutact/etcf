b59f2632f264bcf7edc94196e14f34ba1d4e975c
Ruban Gino Singh <rubangino22@gmail.com>
2024-10-17 19:21:10 -0600

    ADD Files to TEST

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html',
+    './**/*.html',
+    './src/**/*.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#211c5f',
+        secondary: '#4b476c',
+        lightblue: '#59afe0',
+        yellowcolor: '#f5da5b',
+        grey: '#c8cdd5',
+      },
+    },
+  },
+  plugins: [],
+}
+
